4ee81a25b42b47b97e94a6ece67db0c9c01cc762
babu <neupanebabu828@gmail.com>
2022-07-14 22:58:33 +0930

    chaining promises

--- asynchronous/CallbackHell.js
+++ asynchronous/CallbackHell.js
@@ -15,10 +15,19 @@
 //we no longer need to rely on events and callbacks passed into asynchronous function to handle
 //asynchronous result
 
-//consuming promises
+//chaining promises
+
+const renderPost = post => {
+    console.log(post);
+}
 const getCountryData = function () {
     fetch(`https://jsonplaceholder.typicode.com/posts`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data[0]);
+            fetch(`https://jsonplaceholder.typicode.com/posts`)
+                .then(response => response.json())
+                .then(data => renderPost(data[1]))
+        });
 }
 getCountryData();
