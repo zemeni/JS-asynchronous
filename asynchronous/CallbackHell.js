f5e9a81bcdb1f3abdfb01377d5848f289ca52cf8
babu <neupanebabu828@gmail.com>
2022-07-15 00:27:37 +0930

    axios call

--- asynchronous/CallbackHell.js
+++ asynchronous/CallbackHell.js
@@ -17,6 +17,7 @@
 
 //chaining promises
 
+/*
 const renderPost = post => {
     console.log(post);
 }
@@ -25,9 +26,63 @@ const getCountryData = function () {
         .then(response => response.json())
         .then(data => {
             console.log(data[0]);
-            fetch(`https://jsonplaceholder.typicode.com/posts`)
-                .then(response => response.json())
-                .then(data => renderPost(data[1]))
-        });
+            return fetch(`https://jsonplaceholder.typicode.com/posts`);
+        }).then(response => response.json())
+        .then(data => renderPost(data[1]))
 }
-getCountryData();
+getCountryData();*/
+
+//handling rejected promises
+
+/*const renderError = function (err){
+    console.log("error",err.message);
+}
+
+fetch(`https://jsonplaceholder.typicode.com/postsabc`)
+    .then(res => {
+        if(!res.ok)
+            throw new Error(`Posts not found ${res.status}`);
+        return res.json();
+    })
+    .then(data => console.log(data[0]))
+    .catch(err => renderError(err))
+    .finally("finally executed");*/
+/*try {
+    const post = async function () {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/post`);
+        if (!res.ok)
+            throw new Error(`error status ${res.status} and ${res.message}`)
+    }
+} catch
+    (err) {
+
+}
+post().then(data => console.log(data[0]));
+console.log('first');*/
+
+//using axios
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const getPosts = async function (){
+    const {data} = await axios.get(`${BASE_URL}`);
+    console.log(data[0]);
+}
+
+const createPost = async () => {
+    const post = {
+        title:'title',
+        body:'body'
+    };
+    const {data} = await axios.post(BASE_URL, post);
+    console.log(data);
+}
+
+createPost();
+createPost();
+createPost();
+
+getPosts();
+
+
