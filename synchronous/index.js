229c45782f2d6052b861ceacefe665a40e59141e
babu <neupanebabu828@gmail.com>
2022-07-14 20:44:22 +0930

    initial commit

---
+++ synchronous/index.js
@@ -0,0 +1,5 @@
+//this is a synchronous code as it process next line after the complete execution of this line
+const p = document.querySelector("#demo");
+p.textContent="hello there";
+alert("alert box");
+p.style.color="red";
