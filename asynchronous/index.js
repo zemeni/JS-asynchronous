979e6bc1cb0731a69049f92cc9bb80c85173c864
babu <neupanebabu828@gmail.com>
2022-07-14 20:52:18 +0930

    callback func alone doesn't make the code async

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -1,7 +1,11 @@
 //this is asynchronous code, that executes from top to bottom but if found any asynchronous code
 //will jump to the next line leaving the async code to execute in background
+/*
 const p = document.querySelector("#demo");
 setTimeout(()=>{
     p.textContent = 'My name is Babu';
 },5000);
-p.style.color="red";
+p.style.color="red";*/
+
+//call back function alone doesn't make the code async
+// [1,2,3].map(v => console.log(v*2));
