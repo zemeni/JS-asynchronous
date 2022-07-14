dde9b138fb932bcefafb91969612fc1134ffab4c
babu <neupanebabu828@gmail.com>
2022-07-14 21:11:24 +0930

    example 1

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -9,3 +9,13 @@ p.style.color="red";*/
 
 //call back function alone doesn't make the code async
 // [1,2,3].map(v => console.log(v*2));
+
+//in JS loading an image is async
+const img = document.querySelector("#img");
+img.src="https://cdn.pixabay.com/photo/2016/03/30/21/59/coffee-beans-1291656_960_720.jpg";
+// img.src="https://cdn.pixabay.com/photo/2016/04/12/11/19/coffee-1324126_960_720.jpg";
+img.addEventListener('load',() => {
+   img.classList.add('fadeIn');
+   console.log("yes")
+},10000);
+img.style.width='300px';
